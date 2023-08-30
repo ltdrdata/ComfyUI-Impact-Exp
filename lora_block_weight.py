d40db5841bcd86b2a2e912a72dec4370a74a45e3
Dr.Lt.Data <dr.lt.data@gmail.com>
2023-08-30 17:58:27 +0900

    wip: xy plot

--- lora_block_weight.py
+++ lora_block_weight.py
@@ -2,6 +2,14 @@ import folder_paths
 import comfy.utils
 import comfy.lora
 import os
+import torch
+import numpy as np
+import nodes
+from PIL import Image
+
+
+def pil2tensor(image):
+    return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
 
 
 def load_lbw_preset(filename):
@@ -41,7 +49,7 @@ class LoraLoaderBlockWeight:
     RETURN_TYPES = ("MODEL", "CLIP")
     FUNCTION = "doit"
 
-    CATEGORY = "loaders"
+    CATEGORY = "__impact_exp"
 
     @staticmethod
     def load_lora_for_models(model, clip, lora, strength_model, strength_clip, inverse, block_vector):
@@ -143,6 +151,106 @@ class LoraLoaderBlockWeight:
 
         model_lora, clip_lora = LoraLoaderBlockWeight.load_lora_for_models(model, clip, lora, strength_model, strength_clip, inverse, block_vector)
         return (model_lora, clip_lora)
+
+
+class XY_Capsule:
+    def pre_define_model(self, model, clip, vae):
+        return model, clip, vae
+
+    def get_result(self, model, clip, vae):
+        return None
+
+    def set_another_capsule(self, capsule):
+        return None
+
+    def getLabel(self):
+        return "Unknown"
+
+
+class XY_Capsule_LoraBlockWeight(XY_Capsule):
+    def __init__(self, x, y, label, storage):
+        self.x = x
+        self.y = y
+        self.label = label
+        self.storage = []
+        self.another_capsule = None
+
+    def set_another_capsule(self, capsule):
+        self.another_capsule = capsule
+
+    def patch_model(self, model):
+        return model
+
+    def pre_define_model(self, model, clip, vae):
+        if self.y == 0:
+            return self.patch_model(model)
+        elif self.y == 1:
+            return self.patch_model(model)
+        else:
+            return model, clip, vae
+
+    def get_result(self, model, clip, vae):
+        if self.y < 2:
+            return None
+
+        if self.y == 2:
+            # diff
+            weighted_image = self.storage[self.x][0]
+            reference_image = self.storage[self.x][1]
+            image = torch.abs(weighted_image - reference_image)
+            self.storage[self.x][self.y] = image
+        elif self.y == 3:
+            # heatmap
+            heatmap = self.storage[self.x][2].squeeze().sum(dim=-1)
+            image = self.storage[self.x][0].clone()
+            image += 0.5 * heatmap
+
+        latent = nodes.VAEEncode().encode(vae, image)[0]
+        return (latent, image)
+
+
+    def getLabel(self):
+        return self.label
+
+
+class XYPlot_LoraBlockWeight:
+    @classmethod
+    def INPUT_TYPES(cls):
+        preset = ["Preset"]  # 20
+        preset += load_lbw_preset("lbw-preset.txt")
+        preset += load_lbw_preset("lbw-preset.custom.txt")
+
+        default_vectors = "SD-NONE\nSD-ALL\nSD-INS\nSD-IND\nSD-INALL\nSD-MIDD\nSD-MIDD0.2\nSD-MIDD0.8\nSD-MOUT\nSD-OUTD\nSD-OUTS\nSD-OUTALL"
+
+        return {"required": { "model": ("MODEL",),
+                              "clip": ("CLIP", ),
+                              "lora_name": (folder_paths.get_filename_list("loras"), ),
+                              "strength_model": ("FLOAT", {"default": 1.0, "min": -10.0, "max": 10.0, "step": 0.01}),
+                              "strength_clip": ("FLOAT", {"default": 1.0, "min": -10.0, "max": 10.0, "step": 0.01}),
+                              "inverse": ("BOOLEAN", {"default": False, "label_on": "True", "label_off": "False"}),
+                              "preset": (preset,),
+                              "block_vectors": ("STRING", {"multiline": True, "default": default_vectors}),
+                              }}
+
+    RETURN_TYPES = ("XY", "XY"),
+    RETURN_NAMES = ("X (vectors)", "Y (effect_compares)")
+
+    FUNCTION = "xy_value"
+    CATEGORY = "__impact_exp"
+
+    def doit(self, model, clip, lora_name, strength_model, strength_clip, inverse, preset, block_vectors):
+        xy_type = "XY_Capsule"
+
+        xy_value = []
+
+        for block_vectors in preset.split("\n"):
+            block_vector = block_vectors.strip()
+
+            if block_vector == "":
+                XY_Capsule()
+                continue
+
+        return ((xy_type, xy_value), )
 
 
 NODE_CLASS_MAPPINGS = {
